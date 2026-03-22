f3b5d45862cc4d27e59fdbb75ab48d555d05d47e
akshvnd2007-star <akshvnd.2007@gmail.com>
2026-03-22 21:56:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-function showMessage() {
-    alert("Welcome to my portfolio!");
+// Mobile Menu Toggle
+function toggleMenu() {
+    document.querySelector("nav").classList.toggle("active");
 }
+
+// Dark / Light Mode
+function toggleTheme() {
+    document.body.classList.toggle("light");
+}
+
+// Active Navbar Highlight
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 60;
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
